b7b8ca626904119135dbd47b50dcc42b092588ba
darthfederer <142269285+darthfederer@users.noreply.github.com>
2023-10-26 17:25:49 -0700

    Add files via upload

---
+++ Counter_Stress_1_0UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Counter_Stress_1_0UITestsLaunchTests.swift
+//  Counter Stress 1.0UITests
+//
+//  Created by Martin Lim on 10/26/23.
+//
+
+import XCTest
+
+final class Counter_Stress_1_0UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
